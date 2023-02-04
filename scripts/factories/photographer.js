3f8768487f37059bfcc3dfcb519eaa097f345be2
AlexandreSZ <contact.alexandre.szumiel@gmail.com>
2023-02-04 04:25:33 +0100

    modif url

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
   const { name, portrait, city, country, tagline, price, id } = data
 
-  const picture = `../../assets/photographers/Photographers ID Photos/${portrait}`
+  const picture = `./assets/photographers/Photographers ID Photos/${portrait}`
 
   function getUserCardDOM() {
     const article = document.createElement('article')
